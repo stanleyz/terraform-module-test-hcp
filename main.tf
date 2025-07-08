a9832562506c836514258a0292b26dfe25cca7ab
Stanley Zhang <stanley.zhang@ityin.net>
2025-07-08 14:28:01 +1200

    Add module in a sub-folder

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-# module "test" {
-#   source              = "./module"
+module "test" {
+  source              = "./module"
 #   resource_group_name = azurerm_resource_group.example.name
-# }
+}
