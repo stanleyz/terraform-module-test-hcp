d37c915c2730a291a5b37510cd68224b88776ff8
Stanley Zhang <stanley.zhang@ityin.net>
2025-07-08 14:07:09 +1200

    Simple module with git submodule tied to a released version

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "azurerm_resource_group" "example" {
+  name     = "rg-sztest-01"
+  location = "newzealandnorth"
+}
+
 module "test" {
-    source = "./module"
+  source              = "./module"
+  resource_group_name = azurerm_resource_group.example.name
 }
