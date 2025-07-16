e4af84a1f886792e8ccad74e2fb342d7add4d011
Stanley Zhang <stanley.zhang@ityin.net>
2025-07-16 21:51:50 +1200

    use localhost

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "test" {
-  source = "7db4033db65b.ngrok-free.app/azure/network/azurerm"
+  #source = "7db4033db65b.ngrok-free.app/azure/network/azurerm"
+  source = "locahost:8075/azure/network/azurerm"
   version = "2.2.0"
 }
