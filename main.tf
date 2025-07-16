3ae715c251f32bf121f5c603a80dca9ae72a1c66
Stanley Zhang <stanley.zhang@ityin.net>
2025-07-16 22:03:02 +1200

    update backend

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ module "test" {
   source = "7f2ca5b21216.ngrok-free.app/azure/network/azurerm"
   version = "2.2.0"
 }
+
+terraform {
+  cloud {
+    hostname = "20cbb5f32142.ngrok-free.app"
+    organization = "azure"
+
+    workspaces {
+      name = "terraform-module-test-hcp"
+    }
+  }
+}
